dea22fa37d064fd6ca4330de64dff8c621b61196
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2025-08-25 15:58:20 -0700

    Add support for uncompressed textures

--- texture_conversion/dds_to_totexp_p.py
+++ texture_conversion/dds_to_totexp_p.py
@@ -24,9 +24,10 @@ if __name__ == "__main__":
             header['dwSize'], header['dwFlags'], header['dwHeight'], header['dwWidth'],\
                     header['dwPitchOrLinearSize'], header['dwDepth'], header['dwMipMapCount']\
                     = struct.unpack("<7I", img_dat[4:32])
+                tex_type = 0x100 if img_dat[0x54:0x58] == b'\x00\x00\x00\x00' else 0x300 # 1 for uncompressed, 3 for DXT1/DXT5
             with open(tex_file.split('.dds')[0]+'.TOTEXB_D', 'wb') as f:
                 f.write(b'DPDF\x00\x00\x00\x00')
-                f.write(struct.pack("<2QI4HIQ", 0x20, 1, 0xC8, 768, header['dwWidth'], header['dwHeight'], 1, 0, 8))
+                f.write(struct.pack("<2QI4HIQ", 0x20, 1, 0xC8, tex_type, header['dwWidth'], header['dwHeight'], 1, 0, 8))
                 f.write((tex_file.split('.dds')[0]+'.totexp_p').encode()+b'\x00')
             with open(tex_file.split('.dds')[0]+'.TOTEXP_P', 'wb') as f:
                 f.write(struct.pack("<I", len(img_dat)) + img_dat)
